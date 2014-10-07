bd40c02cca8051db2c2df0f9bc32cb0c111f21c0
Dominik "Socek" Dlugajczyk <msocek@gmail.com>
2014-10-07 22:27:55 +0200

    Hatak 0.2.1

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'hatak>=0.2',
+    'hatak>=0.2.1',
     'pyramid_beaker',
 ]
 
 if __name__ == '__main__':
     setup(
         name='Hatak_Beaker',
-        version='0.1.1',
+        version='0.1.2',
         description='Beaker plugin for Hatak.',
         license='Apache License 2.0',
         packages=find_packages('src'),
--- src/haplugin/beaker.py
+++ src/haplugin/beaker.py
@@ -6,5 +6,5 @@ class BeakerPlugin(Plugin):
     def get_include_name(self):
         return 'pyramid_beaker'
 
-    def add_unpackers(self, unpacker):
-        unpacker.add('session', lambda req: req.session)
+    def add_unpackers(self):
+        self.unpacker.add('session', lambda req: req.session)
